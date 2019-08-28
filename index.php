1f2b21c829950992242bb78d1feec9e9e384a3a1
jassgrewallive <54610386+jassgrewallive@users.noreply.github.com>
2019-08-28 08:01:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head> jass </head>
+<body>
+<form action="check.php" method="post">
+username :: <input type="email" name="uname">
+<br><br>
+password :: <input type="password" name="passw">
+<br><br>
+<input type="submit" value"login">
+</form>
+
+</body>
+</html>
